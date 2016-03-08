667641886cb5e33f57fb88524cfeea3dcdf273c9
Greg Pechiro <gregpechiro@yahoo.com>
2016-03-08 11:11:49 -0500

    fully commented javascript

--- static/js/connect4.js
+++ static/js/connect4.js
@@ -1,5 +1,5 @@
-// 263 -
-// initial ize global variables
+// 487 - 263
+// initialize global variables
 
 // this is the total number of squares tall the board will be
 var height = 6;
@@ -64,7 +64,6 @@ function genBoard() {
     } else {
         squareSize = tempSquareSizeW;
     }
-
     */
 
     // once the spuare size is set calulate the total board width
@@ -133,7 +132,7 @@ function genBoard() {
                 button.id = j;
                 button.className += 'insert';
                 // set what happens when the button is clicked
-                // SEE insertClick funciton
+                // SEE insertClick function
                 button.onclick = insertClick;
                 div.appendChild(button);
                 div.style.textAlign = 'center';
@@ -160,7 +159,7 @@ function genBoard() {
 function insertClick() {
     // only proceed if the game is NOT over and if the insert is NOT locked
     if (!gameover && !lock) {
-        // lock the insert so no other insert button can be cicked until it is finished
+        // lock the insert so no other insert button can be clicked until it is finished
         lock = true;
         // get the column number that was clicked from the button id
         var column =+ this.id;
@@ -172,13 +171,13 @@ function insertClick() {
             if (game[row][column] == 0) {
                 // run a function after a certain amount of time to generate a token
                 // the pause is so we can see the token annimation.
-                // the token will appear to be falling by removing generating a token in a square
+                // the token will appear to be falling by generating a token in a square
                 // remove a token from the square above it if there is one
                 // pause 100 miliseconds then repeat
                 // this has to be in an anonymous function to preserve variable state
                 (function (row, column, whosTurn) {
                     window.setTimeout(function() {
-                        // SEE genToken function
+                        // see genToken function
                         genToken(row, column, whosTurn);
                     }, (row * 100));
                 })(row, column, whosTurn);
@@ -200,150 +199,288 @@ function continueInsert(row, column) {
     // insert the players index + 1 at the game row and column
     // (0 is reserved for no mark)
     game[row-1][column] = whosTurn + 1;
+    // increment the number of turns taken
     turns++;
+    // verfify the number of turns taken is greater than or equal to 7
+    // (this is the minimum number of turns needed for a win)
     if (turns >= 7) {
+        // check for a win
+        // see checkWin function
         if (checkWin()) {
+            // in here someone has won
+            // mark the game over
             gameover = true;
+            // display the winner
             updateDisplay('The winner is ' + players[whosTurn].name + '!<br><br>');
+            // display the rematch and restart buttons see genEndGameButtons function
             genEndGameButtons();
+            // unlock
             lock = false;
             return
         }
     }
+    // check for a tie. If the number of turns taken equals the total number
+    // of squares on the board and there is no winner then there is a tie
     if (turns == height * width) {
+        // in here there is a tie
+        // mark the game over
         gameover = true;
+        // display there is  a tie
         updateDisplay('It is a draw!<br><br>');
+        // display the rematch and restart buttons see genEndGameButtons function
         genEndGameButtons();
+        // unlock
         lock = false;
         return;
     }
+    // at this point no one has won and there is no tie
+    // switch whos turcn it is
     whosTurn = (whosTurn == 0) ? 1 : 0;
+    // display whos turns it is
     updateDisplay('It is ' + players[whosTurn].name + '\'s turn');
+    // unlock
     lock = false;
 }
 
+// this function is responsible for annimating the tokens
+// it will draw a token in the givven square then remove
+// a token from the square above it if there is one.
+// this will give the illusion that the token is falling
+// this only draws and removes one token
+// for the full effect we call it in a loop
 function genToken(row, column, whosTurn) {
+    // create the token div and style it
     var token = document.createElement('div');
+    // set the height and width to the square size so the token fills the square
     token.style.height = squareSize + 'px';
     token.style.width = squareSize + 'px';
+    // set the border radius to make it a circle
     token.style.borderRadius = '50%';
+    // color the token to the player who just took their turn's color
     token.style.backgroundColor = players[whosTurn].color;
+    // get the square where we want the token to go
     var square = document.getElementById(row + '-' + column);
+    // append the token to the square
     square.appendChild(token);
+    // check if the row we inserted into is the first row
+    // we want to remove the token above the token we created next
+    // this can only be done if there is a token in the row above this one
+    // the only time there will not be a token above this one is if it is the first row
     if (row > 0) {
+        // in here the row is not the first row
+        // get the square above the current square
         var prevSquare = document.getElementById((row -1) + '-' + column);
+        // remove the inner token from the square
         prevSquare.removeChild(prevSquare.childNodes[0]);
     }
 }
 
+// this function checks if anyone has won
 function checkWin() {
+    // loop all of the rows starting at the bottom instead of the top
+    // the height - 1 will be the id of the bottom row
     for (var row = height - 1; row >= 0; row--) {
+        // for every row loop the columns starting at the left
         for (var column = 0; column < width; column++) {
+            // check if the current row is less than two
+            // if it is less than two then there are at least three rows above it
             if (row > 2) {
-                // compair up
+                // compare up
+                // compare the current square to the squares three rows above it at the same column
                 if (game[row][column] !=0 && game[row][column] == game[row - 1][column] && game[row - 1][column] == game[row - 2][column] && game[row - 2][column] == game[row - 3][column]) {
+                    // if they all match then there is a win
+                    // highlight the matching four
                     document.getElementById(row + '-' + column).style.backgroundColor = 'yellow';
                     document.getElementById((row - 1) + '-' + column).style.backgroundColor = 'yellow';
                     document.getElementById((row - 2) + '-' + column).style.backgroundColor = 'yellow';
                     document.getElementById((row - 3) + '-' + column).style.backgroundColor = 'yellow';
+                    // return true for a win
                     return true;
                 }
             }
+            // check if the current column is less than the width - 3
+            // if it is then there are at least three columns to the right at the same row
             if (column < (width - 3)) {
-                // compair right
+                // compare right
+                // compare the current square to the squares three columns
+                // to the right of it at te same row
                 if (game[row][column] !=0 && game[row][column] == game[row][column + 1] && game[row][column + 1] == game[row][column + 2] && game[row][column + 2] == game[row][column + 3]) {
+                    // if they all match then there is a win
+                    // highlight the matching four
                     document.getElementById(row + '-' + column).style.backgroundColor = 'yellow';
                     document.getElementById(row + '-' + (column + 1)).style.backgroundColor = 'yellow';
                     document.getElementById(row + '-' + (column + 2)).style.backgroundColor = 'yellow';
                     document.getElementById(row + '-' + (column + 3)).style.backgroundColor = 'yellow';
+                    // return true for a win
                     return true;
                 }
             }
+            // check if the current row is less than two and
+            // check if the current column is less than the width - 3
+            // if both are true then there are at least three rows above and
+            // at least three rows to the right
             if (row > 2 && column < (width - 3)) {
                 // compair up-right diagonal
+                // compare the current square to the three squares, up one row
+                //and right one column each square
                 if (game[row][column] !=0 && game[row][column] == game[row - 1][column + 1] && game[row - 1][column + 1] == game[row - 2][column + 2] && game[row - 2][column + 2] == game[row - 3][column + 3]) {
+                    // if they all match then there is a win
+                    // highlight the matching four
                     document.getElementById(row + '-' + column).style.backgroundColor = 'yellow';
                     document.getElementById((row - 1) + '-' + (column + 1)).style.backgroundColor = 'yellow';
                     document.getElementById((row - 2) + '-' + (column + 2)).style.backgroundColor = 'yellow';
                     document.getElementById((row - 3) + '-' + (column + 3)).style.backgroundColor = 'yellow';
+                    // return true for a win
                     return true;
                 }
             }
+            // check if the current row is less than height - 3 and
+            // check if the current column is less than the width - 3
+            // if both are true then there are at least three rows below and
+            // at least three rows to the right
             if (row < (height - 3) && column < (width-3)) {
                 // compair down-right diagonal
+                // compare the current square to the three squares, down one row
+                //and right one column each square
                 if (game[row][column] !=0 && game[row][column] == game[row + 1][column + 1] && game[row + 1][column + 1] == game[row + 2][column + 2] && game[row + 2][column + 2] == game[row + 3][column + 3]) {
+                    // if they all match then there is a win
+                    // highlight the matching four
                     document.getElementById(row + '-' + column).style.backgroundColor = 'yellow';
                     document.getElementById((row + 1) + '-' + (column + 1)).style.backgroundColor = 'yellow';
                     document.getElementById((row + 2) + '-' + (column + 2)).style.backgroundColor = 'yellow';
                     document.getElementById((row + 3) + '-' + (column + 3)).style.backgroundColor = 'yellow';
+                    // return true for a win
                     return true;
                 }
             }
         }
     }
+    // if we are here the there is no win yet
+    // return false for no win
     return false;
 }
 
+// this function displays any text passed to it in the display div
+// it will also color the message to the current players color
 function updateDisplay(message) {
+    // get the display div
     var display = document.getElementById('display');
+    // set the color to the current playes div
     display.style.color = players[whosTurn].color;
+    // set the text to the message passed in
     display.innerHTML = message;
 }
 
+
+// this function creates the rematch and restart buttons after the game has ended
 function genEndGameButtons() {
+    // get the display element
     var display = document.getElementById('display');
+    // create the rematch button
     var rematchButton = document.createElement('button');
+    // add text to the button
     rematchButton.innerText = 'Rematch';
+    // add a click action to the button
     rematchButton.onclick = function() {
+        // see drop function
         drop(0);
     }
+    // add the button to the display
     display.appendChild(rematchButton);
+    // create the restart button
     var startOverButton = document.createElement('button');
+    // add text to the button
     startOverButton.innerText = 'Start Over';
+    // add a click action to the button see startOver function
     startOverButton.onclick = startOver;
+    // add the button to the display
     display.appendChild(startOverButton);
 }
 
+// this function creates the end of game annimation
+// it looks as if the bottomwas removed and all of the tokens dropped down
+// like the real connect 4 game
+// this is a recursive function it needs to run a number of times equal to the
+// number of rows/height of the game board
+// every time this is called it is passed the number of time it already has been called
+// it starts in one square and will draw the token in the square above it into itself
+// then loop over all of  the squares from the bottom right
 function drop(count) {
+    // loop over the height
     for (var row = height-1; row >= 0; row--) {
+        // for every row loop over the width
         for (var column = 0; column < width; column++) {
+            // get the square located at the row and column
             var square = document.getElementById(row + '-' + column);
+            // setup the toen and background variables
             var token = '';
             var backGround = '';
+            // check if we are NOT in the top row
             if (row > 0) {
+                // get the square above the current square in the same column
                 var prevSquare = document.getElementById((row - 1) + "-" + column);
+                // set the token to the previous square's token
                 token = prevSquare.innerHTML;
+                // set the background to the previous square's background
+                // this will allow the win highlight to drop as well
                 backGround = prevSquare.style.backgroundColor;
             }
+            // set the current square's token to the token
+            // this will either be the a token or empty
             square.innerHTML = token;
+            // set the current square's BACKGROUND to the background
+            // this will either be the a the win hightlight or empty
             square.style.backgroundColor = backGround;
         }
     }
+    // add to the number of times we ran this function
     count++;
+    // check if the count is greater than the height -1
     if (count > height -1) {
+        // switch the players turn
         whosTurn = (whosTurn == 0) ? 1 : 0;
+        // restart the game see  restart function
         restart()
+        // exit the fdrop function
         return
     }
+    // if here then the count is not greater than the height - 1
     window.setTimeout(function() {
+        // run the drop function again after a given amount of time
+        // passing the updated count
         drop(count)
+        // the delay allows the tokens to be seen as they are dropping
     }, 150);
 }
 
+// this function restarts the game with the same settings
 function restart() {
+    // reset the game dta variable
     game = [];
+    // clear the action button area
     document.getElementById('action').innerHTML = '';
+    // clear the board area
     document.getElementById('board').innerHTML = '';
+    // set the yurns to zero
     turns = 0;
+    // mark game over to false
     gameover = false;
+    // re-generate the game board with the current settings
     genBoard();
+    // display the cirrent players turn
     updateDisplay('It is ' + players[whosTurn].name + '\'s turn');
 }
 
+// this function will allow the players to completely start over`
+// they will be presented with the form originally used to set up the game
+// the previous games values with still be the starting form values
 function startOver() {
+    // get the board
     var board = document.getElementById('game');
+    // hide the board by by not displaying it
     board.style.display = 'none';
-
+    // get the setup form
     var setup = document.getElementById('setup');
+    // show the setup form by adding a display type
     setup.style.display = 'block';
 }
--- static/js/setup.js
+++ static/js/setup.js
@@ -1,78 +1,136 @@
 
+// create the start game button
 var startButton = document.getElementById('startGame');
+
+// add a click action to the start button
 startButton.onclick = function() {
+    // run function on click see startGame function
     startGame();
 }
 
+// this isthe start game function
+// it setups up the variables needed based on the user input
+// then triggers the game tostart
 function startGame() {
+    // get the error div
     var error = document.getElementById('setupError');
+    // set the text to nothing
     error.innerText = '';
+    // get player 1 name from the input value
     var player1Name = document.getElementById('p1Name').value;
+    // check if the player 1 name is empty
     if (player1Name == '') {
+        // set the error for an empty input
         error.innerText = 'All fields must be filled out';
+        // exit funciton
         return
     }
+    // get player 2 name from the input value
     var player2Name = document.getElementById('p2Name').value;
+    // check if the player 2 name is empty
     if (player2Name == '') {
+        // set the error for an empty input
         error.innerText = 'All fields must be filled out';
+        // exit the function
         return
     }
+    // check if the player names are the same
     if (player1Name == player2Name) {
+        // set the error for matching names
         error.innerText = 'Player names must be different';
+        // exit the function
         return
     }
-
+    // get the height of the board from the input
+    // the `=+` is a little trick that will convert the string from the input
+    // to a javascript nuber
     height =+ document.getElementById('height').value;
+    // check if the height is empty
     if (height == '') {
+        // set the error for an emty input
         error.innerText = 'All fields must be filled out';
+        // exit the function
         return
     }
+    // check if the height is less than 4
     if (height < 4) {
+        // set the error for minimun dimensions
         error.innerText = 'Minimum dimensions are 4 x 4';
+        // exit the function
         return
     }
+    // get the width of the board from the input
+    // the `=+` is a little trick that will convert the string from the input
+    // to a javascript nuber
     width =+ document.getElementById('width').value;
+    // check if the width is empty
     if (width == '') {
+        // set the error for an empty input
         error.innerText = 'All fields must be filled out';
+        // exit the funciton
         return
     }
+    // check if the width is less than 4
     if (width < 4) {
+        // set the error for minimum dimensions
         error.innerText = 'Minimum dimensions are 4 x 4';
+        // exit the function
         return
     }
+    // check if the width is greater than 16
     if (width > 16) {
+        // set the error for maximum dimensions
         error.innerText = 'Maximum dimensions are 11 x 16';
+        // exit the function
         return
     }
+    // check if the height is greater than 11
     if (height > 11) {
+        // set the error for maxiun dimensions
         error.innerText = 'Maximum dimensions are 11 x 16';
+        // exit the function
         return
     }
-
+    // get both players' colors from the inputs
     var player1Color = document.getElementById('p1Color').value;
     var player2Color = document.getElementById('p2Color').value;
+    // check if the colors are the same
     if (player1Color == player2Color) {
+        // set the error for matching colors
         error.innerText = 'Player colors must be different';
+        // exit the function
         return
     }
+    // get the starting player radio buttons
     var starters = document.querySelectorAll('#whoStarts');
+    // loop over list of players
     for (var i = 0; i < starters.length; i++) {
+        // check if the current player is checked
         if (starters[i].checked) {
+            // get the player who will start from the input
             whosTurn =+ starters[i].value;
+            // exit the for loop
             break;
         }
     }
+    // set the players' names and colors into the player
+    // setup list of maps
     players[0].name = player1Name;
     players[0].color = player1Color;
     players[1].name = player2Name;
     players[1].color = player2Color;
 
+    // get the setup div
     var setup = document.getElementById('setup');
+    // hide the setup by setting the display to none
     setup.style.display = 'none';
 
+    // get the board div
     var board = document.getElementById('game');
+    // show the board div by giving the display property a value
     board.style.display = 'block';
 
+    // run the restart function which will also start the game
+    // see restart function
     restart();
-
 }
